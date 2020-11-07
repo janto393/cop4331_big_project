2bcfefda9a99cd5d50b810e14affd6dc2df970e0
cromero048 (Camilo Romero) <cromero048@knights.ucf.edu>
2020-11-06 21:54:09 -0500

    reinitialize react app

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import MyRecipesPage from './pages/MyRecipesPage';
+
+function App() {
+  return (
+    <MyRecipesPage />
+  );
+}
+
+export default App;
---
+++ front-end/src/pages/MyRecipesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import PageTitle from '../components/PageTitle';
+// import Login from '../components/Login';
+
+const MyRecipesPage = () =>
+{
+
+    return(
+      <div>
+      </div>
+    );
+};
+
+export default MyRecipesPage;
