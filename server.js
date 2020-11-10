4fef6743e590375dd956067d11e9ef339b95e7df
broseph99 <70461498+broseph99@users.noreply.github.com>
2020-11-09 22:43:35 -0500

    create recipe api uploaded, still in works

--- server.js
+++ server.js
@@ -34,47 +34,108 @@ app.use((req, res, next) =>
   next();
 });
 
-// update user Endpoint
-app.post('/updateUser', async (request, response, next) =>
+// Create new recipe endpoint. 
+app.post('/createRecipe', async (request, response, next) =>
 {
-  // incoming: userId, username, firstName, lastName, profilePicture
-  // outgoing: userID, success
+  // incoming: userID, publicRecipe, isMetric, title, instructions, ingredientsByRecipe
+  // outgoing: recipeID, ingredients, error
 	
-  const { 
-          userID, 
-          username,
-          password,
-					firstName,
-					lastName,
-					// profilePicture
+  const { userID,
+          publicRecipe,
+          isMetric,
+          title,
+          instructions,
+          ingredientsByRecipe
 				} = request.body;
 
-  const INVALID_USER = -1;
+  const INVALID_RECIPE = -1;
   var result = null;
-  var success;
 
-  var searchUser = {
-                      userID : userID
-                  };
   var returnPackage = {
-                        userID : INVALID_USER,
-                        success : false
+                        recipeID : INVALID_RECIPE,
+                        ingredients : [],
+                        error : ''
                       };
 
-  const updateUser = { $set:{
-                      username : username,
-                      password: password,
-                      firstName : firstName,
-                      lastName : lastName,
-                      // profilePicture : profilePicture,
-                  }};
+  const newRecipe = {
+                    title : title, 
+                    publicRecipe : publicRecipe, 
+                    isMetric : isMetric,
+                    instructions : instructions,
+                    ingredientsByRecipe : ingredientsByRecipe
+                  };
 
-  // Update user record. 
+  // Check if recipe name is already taken.
+  try 
+  {
+		const db = client.db(process.env.APP_DATABASE);
+		
+    const criteria = {
+                        title : title
+                      };
+		const data = await db.collection(process.env.COLLECTION_RECIPES).findOne(criteria);
+		
+    if (data)
+    {
+      throw "Recipe name taken.";
+    }
+  }
+  catch(e)
+  {
+    returnPackage.error = e.toString();
+    response.status(400).json(returnPackage);
+    return;
+  }
+
+  // Seperate function to insert incredients to db if they don't exist already. 
+  // Travse through array of objects 
+  var i;
+  for (i = 0; i < ingredientsByRecipe.length; i++)
+  {
+    var searchInd = {
+                      name : ingredientsByRecipe[i].name
+                    };
+
+    try
+    {
+      const db = client.db(process.env.APP_DATABASE);
+      var result = await db.collection(process.env.COLLECTION_INGRDIENTS).findOne(searchInd);
+    }
+    catch(e)
+    {
+      console.log(e.toString());
+    }
+    // If ingredient exist then don't add to list again.
+    if (result)
+    {
+      continue;
+    }
+    // If ingredient doesn't exist, add to collection. 
+    else
+    {
+      // Essentially same code as for register. Add seperate function.
+      try
+      {
+        const db = client.db(process.env.APP_DATABASE);
+        db.collection(process.env.COLLECTION_INGRDIENTS).insertOne(ingredientsByRecipe[i]);
+
+        var result = await db.collection(process.env.COLLECTION_INGRDIENTS).findOne(ingredientsByRecipe[i]);
+      }
+      catch(e)
+      {
+        console.log(e.toString());
+      }
+    }
+  }
+  
+
+  // Inserts new recipe record.
   try
   {
     const db = client.db(process.env.APP_DATABASE);
-    var result = await db.collection(process.env.COLLECTION_USERS).updateOne(searchUser, updateUser);
-    
+    db.collection(process.env.COLLECTION_RECIPES).insertOne(newRecipe);
+
+		var result = await db.collection(process.env.COLLECTION_RECIPES).findOne(newRecipe);
   }
   catch(e)
   {
@@ -82,15 +143,19 @@ app.post('/updateUser', async (request, response, next) =>
     response.status(500).json(returnPackage);
     return;
   }
-  console.log(result);
+
   // Assigns return package values. 
   if (result)
   {
-    returnPackage.userID = result.userID;
-    success = true;
-    returnPackage.success = success;
+    returnPackage.recipeID = result.userID;
+    returnPackage.ingredients = result.ingredients;
   }
   response.status(200).json(returnPackage);
-});
+}
+);
+app.listen(5000); // start Node + Express server on port 5000
+
+
+
 
 app.listen(process.env.PORT || 5000); // start Node + Express server on port 5000
