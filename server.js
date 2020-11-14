62c929b31a25b23d3556969bbacf000782b7a2f0
broseph99 <70461498+broseph99@users.noreply.github.com>
2020-11-14 17:34:05 -0500

    Function logn endpoint

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const express = require('express');
+const expresponses = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const objectID = require('mongodb').ObjectID;
 const path = require('path');
 
-const app = express();
+const app = expresponses();
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -21,76 +21,46 @@ const client = new MongoClient(url, { useUnifiedTopology: true });
 client.connect();
 
 // Access Control Logic
-app.use((req, res, next) => 
+app.use((request, response, next) => 
 {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-requestuested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader(
+  response.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
 });
 
-// Create new recipe endpoint. 
-app.post('/createRecipe', async (request, response, next) =>
+// Login endpoint. 
+app.post('/login', async (request, response, next) => 
 {
-  // incoming: userID, publicRecipe, isMetric, title, instructions, ingredientsByRecipe
-  // outgoing: recipeID, ingredients, error
+  // incoming: username, password
+  // outgoing: userID, username, email, firstName, lastName, profilePicture
+  // isVerified, favoriteRecipes, error
 
-  // JSON query package
-  // {
-  //   "author": "5fa613c408e77205a8e4aebe",
-  //   "publicRecipe": false,
-  //   "isMetric": true,
-  //   "title": "Gingerbread man",
-  //   "instructions": ["Mix ingredients", "Bake for 10 mins"],
-  //   "ingredients": [{}]
-  // }
-	
-  const { publicRecipe,
-          isMetric,
-          title,
-          author,
-          instructions,
-          ingredients
-				} = request.body;
+ var error = '';
+ const { username, password } = request.body;
+ var userID = -1;
+ var email = '';
+ var firstName = '';
+ var lastName = '';
+//  var profilePicture = ''; // Does picture get stored in database as a string?
+ var isVerified = false;
+ var favoriteRecipes = null;
 
-  const INVALID_RECIPE = -1;
-  var result = null;
-  var authorID = objectID(author);
-
-  var returnPackage = {
-                        recipeID : INVALID_RECIPE,
-                        ingredients : [],
-                        error : ''
-                      };
-
-  const newRecipe = {
-                    title : title, 
-                    author : authorID,  // user id
-                    publicRecipe : publicRecipe, 
-                    isMetric : isMetric,
-                    instructions : instructions, // array of strings with each index representing a step.
-                    ingredients : [] // array of objects
-                  };
-  // Check if recipe name is already taken.
-  try 
+ try 
   {
 		const db = client.db(process.env.APP_DATABASE);
 		
     const criteria = {
-                        title : title
+                      username:username, 
+                      password:password
                       };
-		const data = await db.collection(process.env.COLLECTION_RECIPES).findOne(criteria);
-		
-    if (data)
-    {
-      throw "Recipe name taken.";
-    }
+		var results = await db.collection(process.env.COLLECTION_USERS).findOne(criteria);
   }
   catch(e)
   {
@@ -98,55 +68,32 @@ app.post('/createRecipe', async (request, response, next) =>
     response.status(400).json(returnPackage);
     return;
   }
- 
-  // Travse through array of objects 
-  // 1. Check if ingredients are in database.
-  //    -if yes, grab objectid and add into json
-  //    -if no, insert into ingredients database and grab objectid
-  // 2. Check value of ismetric
-  //    -if yes, insert amount and search for metric unit in Unit_metric database.
-  //    -if no, insert amount and search for imperial unit in Unit_imperial database.
 
+ console.log(results);
+ if( results )
+	{
+		userID = results.userID;
+		email = results.email;
+		firstName = results.firstName;
+		lastName = results.lastName;
+		// profilePicture = results.profilePicture;
+		isVerified = results.isVerified;
+		favoriteRecipes = results.favoriteRecipes;
+	}
 
-  // ingredientsByRecipe api?
-  // var i;
-  // var name;
-  // var amount;
-  // var units;
-  // for (i = 0; i < ingredients.length; i++)
-  // {
-  //   name = objectID(ingredients[i].name);
-  //   amount = ingredients[i].amount;
-  //   units = objectID(ingredients[i].units);
-  //   newRecipe.ingredients[i] = {name : name, amount : amount, units : units}
-  //   console.log(newRecipe.ingredients[i]);
-  // }
-  
+  var ret = {
+							userID : userID,
+							username : results.username,
+							email : email,
+							firstName : firstName,
+							lastName:lastName,
+							// profilePicture : profilePicture,
+							isVerified : isVerified,
+							favoriteRecipes : favoriteRecipes,
+							error : ''
+						};
 
-  // Inserts new recipe record.
-  try
-  {
-    const db = client.db(process.env.APP_DATABASE);
-    db.collection(process.env.COLLECTION_RECIPES).insertOne(newRecipe);
+  response.status(200).json(ret);
+});
 
-		var result = await db.collection(process.env.COLLECTION_RECIPES).findOne(newRecipe);
-  }
-  catch(e)
-  {
-    returnPackage.error = e.toString();
-    response.status(500).json(returnPackage);
-    return;
-  }
-console.log(result);
-  // Assigns return package values. 
-  if (result)
-  {
-    returnPackage.recipeID = result.userID;
-    returnPackage.ingredients = result.ingredients;
-  }
-  response.status(200).json(returnPackage);
-}
-);
-app.listen(5000); // start Node + Express server on port 5000
-
-app.listen(process.env.PORT || 5000); // start Node + Express server on port 5000
+app.listen(process.env.PORT || 5000); // start Node + Expresponses server on port 5000
