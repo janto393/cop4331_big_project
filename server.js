0327d406401a6834af46a46924d49d58c7c7d732
broseph99 <70461498+broseph99@users.noreply.github.com>
2020-11-18 01:24:15 -0500

    Functioning endpoint

--- server.js
+++ server.js
@@ -35,52 +35,65 @@ app.use((req, res, next) =>
 });
 
 
-// delete recipe endpoint
-app.post('/deleteRecipe', async (request, response, next) =>
+// edit favorite recipes Endpoint
+app.post('/editFavoriteRecipes', async (request, response, next) =>
 {
-  // incoming: recipeID
-  // outgoing: success, error
-  // JSON package exmaple
-  // {
-  //   "_id": "5fb332c66176de0b60a35c69"
-  // }
+  // incoming: _id, favoriteRecipes (_ids' of recipes are inserted as strings and api endpoint converts into objectid)
+  // outgoing: _id, success, error
+  // JSON package example 
+//   {
+// 	"_id": "5fa613c408e77205a8e4aebe",
+// 	"favoriteRecipes": [
+// 	  "5fb48f7dff9d47012c545228"
+// 	]
+//   }
 
   var searchUser = {
-					_id : ObjectID(request.body._id)
+					_id : objectID(request.body._id),
   };
+  const INVALID_USER = -1;
   var result = null;
 
   var returnPackage = {
+                        _id : INVALID_USER,
 						success : false,
 						error : ''
-					  };
-					  
-					  
+                      };
 
-  // Delete recipe record. 
+  
+	var i;
+	var fave = [];
+	for (i = 0; i < request.body.favoriteRecipes.length; i++)
+	{
+		fave.push(objectID(request.body.favoriteRecipes[i]));
+		
+	}
+
+	var updateUser = { $set:{
+								favoriteRecipes : fave
+	}};
+	
+  // Update user record. 
   try
   {
     const db = client.db(process.env.APP_DATABASE);
-	await db.collection(process.env.COLLECTION_RECIPES).deleteOne(searchUser);
-	var result = await db.collection(process.env.COLLECTION_RECIPES).findOne(searchUser);
+		await db.collection(process.env.COLLECTION_USERS).updateOne(searchUser, updateUser);
+		var result = await db.collection(process.env.COLLECTION_USERS).findOne(searchUser);
   }
   catch(e)
   {
     returnPackage.error = e.toString();
     response.status(500).json(returnPackage);
     return;
-  }
-  
-  console.log( result);
+	}
+console.log(result);
   // Assigns return package values. 
-  if (!result)
+  if (result)
   {
-    
+    returnPackage._id = result._id;
     returnPackage.success = true;
-    // console.log(returnPackage.success);
 	}
 
   response.status(200).json(returnPackage);
 });
-
 app.listen(process.env.PORT || 5000); // start Node + Express server on port 5000
