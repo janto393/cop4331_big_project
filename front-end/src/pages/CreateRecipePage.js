322864094b9f537303773f277cdc995ae0866839
cromero048 (Camilo Romero) <cromero048@knights.ucf.edu>
2020-11-18 03:56:43 -0500

    better progress on CreateRecipe

--- front-end/src/pages/CreateRecipePage.js
+++ front-end/src/pages/CreateRecipePage.js
@@ -8,10 +8,8 @@ const CreateRecipePage = () =>{
 
     const [message,setMessage] = useState('');
 
-    const [instructionList, setInstructionList] = useState([]);
-    const [ingredientList, setIngredientList] = useState([]);
+    const [publicize,setPublicize] = useState(false)
 
-    const [publicize,setPublicize] = useState(false);
     const isPublicized = () => setPublicize(!publicize);
 
     var newRecipe = {
@@ -46,8 +44,12 @@ const CreateRecipePage = () =>{
 
         try
         {
+          
+          newRecipe.publicRecipe = publicize;
+          newRecipe.ingredients = ingredientList;
           newRecipe.instructions = instructionList;
           console.log(newRecipe);
+
             // const response = await fetch( buildPath('/createRecipe'),
             // {method:'POST',body:JSON.stringify(newRecipe),headers:{'Content-Type': 'application/json'}});
             
@@ -69,40 +71,49 @@ const CreateRecipePage = () =>{
         {
             setMessage(e.toString());
         }
-
   };
 
-  // -===== Ingredients Changes =====-
+  const [instructionList, setInstructionList] = useState([{ instruction: "" }]);
+  const [ingredientList, setIngredientList] = useState([{ ingredient: "", quantity: "" }]);
+
   // handle input change
-  const handleIngredientChange = (ing, index) => {
-    const { ingred, value } = ing.target;
+  const handleIngredientChange = (e, index) => {
+    const { name, value } = e.target;
     const list = [...ingredientList];
-    list[index][ingred] = value;
+    list[index][name] = value;
     setIngredientList(list);
   };
+
   // handle click event of the Remove button
   const handleRemoveClick = index => {
     const list = [...ingredientList];
     list.splice(index, 1);
     setIngredientList(list);
   };
-  
+
   // handle click event of the Add button
   const handleAddClick = () => {
-    setIngredientList([...ingredientList, { ingredients: "" }]);
+    setIngredientList([...ingredientList, { ingredient: "" }]);
   };
 
-  // -===== Instructions Changes =====-
+  // handle input change
+  const handleInstructionChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...instructionList];
+    list[index][name] = value;
+    setInstructionList(list);
+  };
+
   // handle click event of the Remove button
   const handleRemoveClickIns = index => {
     const list = [...instructionList];
     list.splice(index, 1);
     setInstructionList(list);
   };
-  
+
   // handle click event of the Add button
   const handleAddClickIns = () => {
-    setInstructionList([...instructionList, { instructions: "" }]);
+    setInstructionList([...instructionList, { instruction: "" }]);
   };
 
     return(
@@ -120,50 +131,60 @@ const CreateRecipePage = () =>{
               </Col>
             </Form.Group>
 
-            <Form.Group as={Row} controlId="formHorizontalPassword">
+            <Form.Group as={Row} controlId="formBasicText">
               <Form.Label column = "lg" sm={2}>
               Instructions
               </Form.Label>
-            {instructionList.map((newInstruction,i) => {
+              <Col lg={9}>
+              {instructionList.map((x,i) => {
                   return(
                       <div key={i}>
-                        <Form.Control size="lg" placeholder="Instructions" value={newInstruction} onChange={(newInstruction) => {instructionList[i] = newInstruction}}/>
-                        {instructionList.length-1 === i && i !== 0 && <Button variant="primary" onClick={() => handleRemoveClickIns(i)}>Remove</Button>}
-                        {instructionList.length-1 === i && <Button variant="primary" onClick={() => handleAddClickIns(i)}>Add</Button>}
+                        <Form.Control  type ="text" name="instruction" size="lg" placeholder="Instructions" value={x.instruction} onChange={e => handleInstructionChange(e,i)}/>
+                        {instructionList.length - 1 === i && i !== 0 && <Button onClick={() => handleRemoveClickIns(i)}>Remove</Button>}
+                        {instructionList.length - 1 === i && <Button onClick={handleAddClickIns}>Add</Button>}
                       </div>
                   );
               })}
+              </Col>
             </Form.Group>
 
-            <Form.Group as={Row} controlId="formHorizontalPassword">
+            <Form.Group as={Row} controlId="formBasicText">
               <Form.Label column = "lg" sm={2}>
               Ingredients
               </Form.Label>
               <Col lg={4}>
-              {ingredientList.map((x,i)=>{
-                  return(
-                      <div key={i}>                        
-                        <Form.Control size="lg" placeholder="Ingredients" value={x.ingredients} onChange={e => handleIngredientChange(e, i)}/>
-                        <Form.Control size="lg" placeholder="Quantity" value={x.quantity} onChange={e => handleIngredientChange(e, i)}/>
-                        {ingredientList.length-1 === i && i !== 0 && <Button variant="primary" onClick={() => handleRemoveClick(i)}>Remove</Button>}
-                        {ingredientList.length-1 === i && <Button  variant="primary" onClick={() => handleAddClick(i)}>Add</Button>}
-                      </div>
-                  );
+              {ingredientList.map((x, i) => {
+                return (
+                  <div key={i}>
+                    <Form.Control type ="text" name="ingredient" placeholder="Enter Ingredient" value={x.ingredient} onChange={e => handleIngredientChange(e, i)} />
+                    <Form.Control type ="text" name="quantity" placeholder="Enter Numeric Quantity" value={x.quantity} onChange={e => handleIngredientChange(e, i)} />
+                    {ingredientList.length - 1 === i && i !== 0 && <Button onClick={() => handleRemoveClick(i)}>Remove</Button>}
+                    {ingredientList.length - 1 === i && <Button onClick={handleAddClick}>Add</Button>}
+                  </div>
+                );
               })}
+              <Col lg={5}>
+              <Form.Group as={Row}controlId="formBasicCheckbox">
+                <Form.Check label="Metric Units" type="checkbox" size="lg" onClick={isPublicized}/>
+                <Form.Check label="Imperial Units" type="checkbox" size="lg" onClick={isPublicized}/>
+              </Form.Group>
               </Col>
+              <Form.Group as={Row} controlId="formBasicCheck">
+                <Col lg={{ span: 10}}>
+                <Form.Check label="Private Recipe" type="switch" size="lg" onClick={isPublicized}/>
+                </Col>
+              </Form.Group>
+            </Col>
             </Form.Group>
 
-            <Form.Group as={Row} controlId="formHorizontalCheck">
-              <Col lg={{ span: 10, offset: 2 }}>
-                {/* <Form.Check label="Private" type="switch" size="lg" onClick={isPublicized} ref={(c) => publicRecipe = c}/> */}
-              </Col>
-            </Form.Group>
 
             <Form.Group as={Row}>
               <Col sm={{ span: 10, offset: 2 }}>
                 <Button type="submit" onClick={addRecipe} >Create</Button>
               </Col>
             </Form.Group>
+            {/* <div style={{ marginTop: 20 }}>{JSON.stringify(instructionList)}</div> */}
+            {/* <div style={{ marginTop: 20 }}>{JSON.stringify(ingredientList)}</div> */}
 
           </Form>
         </div>
