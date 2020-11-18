2724a2b4b39156b3c5d9f24140aba9cb4cb3921b
broseph99 <70461498+broseph99@users.noreply.github.com>
2020-11-18 01:21:29 -0500

    functioning delete recipe endpoint

--- server.js
+++ server.js
@@ -34,391 +34,35 @@ app.use((req, res, next) =>
   next();
 });
 
-// Add Ingredient Endpoint
-app.post('/addIngredient', async (request, response, next) =>
+
+// delete recipe endpoint
+app.post('/deleteRecipe', async (request, response, next) =>
 {
-	/*
-		Incoming:
-		{
-			name : string
-		}
-		Outgoing:
-		{
-			ingredientID : objectID,
-			success : bool,
-			error : ''
-		}
-	*/
+  // incoming: recipeID
+  // outgoing: success, error
+  // JSON package exmaple
+  // {
+  //   "_id": "5fb332c66176de0b60a35c69"
+  // }
 
-	var returnPackage = {
-												ingredientID : null,
-												success : false,
-												error : ''
-											};
-
-	// ensure we are not inserting a duplicate
-	try
-	{
-		const db = await client.db(process.env.APP_DATABASE);
-
-		const criteria = {
-											 name : request.body.name.toLowerCase()
-										 };
-
-		var result = await db.collection(process.env.COLLECTION_INGREDIENTS).findOne(criteria);
-
-		// exit if ingredient is ready added
-		if (result)
-		{
-			returnPackage.ingredientID = result._id;
-			returnPackage.success = true;
-			response.status(200).json(returnPackage);
-			return;
-		}
-	}
-	catch (e)
-	{
-		returnPackage.error = e.toString();
-		response.status(500).json(returnPackage);
-		return;
-	}
-
-	// insert the ingredient to the database
-	try
-	{
-		const db = await client.db(process.env.APP_DATABASE);
-		
-		const criteria = {
-											 name : request.body.name.toLowerCase()
-										 };
-
-		db.collection(process.env.COLLECTION_INGREDIENTS).insertOne(criteria);
-
-		// need to search it in the database to get the id of the ingredient
-		var result = await db.collection(process.env.COLLECTION_INGREDIENTS).findOne(criteria);
-
-		returnPackage.ingredientID = result._id;
-		returnPackage.success = true;
-	}
-	catch (e)
-	{
-		returnPackage.error = e.toString();
-		response.status(500).json(returnPackage);
-		return;
-	}
-
-	response.status(200).json(returnPackage);
-});
-
-
-// Fetches all units depending on system selected
-app.post('/fetchUnits', async (request, response, next) => {
-	/*
-		Incoming:
-		{
-			isMetric : bool
-		}
-
-		Outgoing:
-		{
-			units : array,
-			error : string
-		}
-	*/
-
-	var collection;
-
-	var returnPackage = {
-												units : [],
-												error : ''
-											};
-
-
-	// Determine collection to pull from
-	if (request.body.isMetric)
-	{
-		collection = process.env.COLLECTION_METRIC_UNITS;
-	}
-	else
-	{
-		collection = process.env.COLLECTION_IMPERIAL_UNITS;
-	}
-
-	// Fetch the units from the database
-	try
-	{
-		const db = await client.db(process.env.APP_DATABASE);
-
-		const data = await db.collection(collection).find().toArray();
-
-		// exit if no units were returned
-		if (!data)
-		{
-			returnPackage.error = 'No units returned from database.';
-			response.status(400).json(returnPackage);
-			return;
-		}
-
-		returnPackage.units = data;
-	}
-	catch (e)
-	{
-		returnPackage.error = e.toString();
-		response.status(500).json(returnPackage);
-		return;
-	}
-
-	response.status(200).json(returnPackage);
-});
-
-
-// Find Ingredient Endpoint
-app.post('/findIngredient', async (request, response, next) => {
-	/*
-		Incoming:
-		{
-			name : string
-		}
-
-		Outgoing:
-		{
-			ingredientID : string,
-			name : string,
-			error : string
-		}
-	*/
-
-	returnPackage = {
-										ingredientID : '',
-										name : '',
-										error : ''
-									};
-
-	// look for ingredient in database
-	try
-	{
-		const db = await client.db(process.env.APP_DATABASE);
-
-		const criteria = {
-											 name : request.body.name.toLowerCase()
-										 };
-
-		var result = await db.collection(process.env.COLLECTION_INGREDIENTS).findOne(criteria);
-
-		if (result)
-		{
-			returnPackage.ingredientID = result._id;
-			returnPackage.name = result.name;
-		}
-	}
-	catch (e)
-	{
-		returnPackage.error = e.toString();
-		response.status(500).json(returnPackage);
-		return;
-	}
-
-	response.status(200).json(returnPackage);
-});
-
-
-// Matches imperial to metric units for conversion package
-app.post('/fromImperialToMetric', async (request, response, next) =>
-{
-	/*
-		Icomming:
-		{
-			unit : string
-		}
-
-		Outgoing:
-		{
-			unit : string,
-			error : string
-		}
-	*/
-
-	var returnPackage = {
-												unit : '',
-												error : ''
-											};
-
-	// lb to kg
-	if (request.body.unit == 'lb')
-	{
-		returnPackage.unit = 'kg';
-	}
-
-	// oz to g
-	else if (request.body.unit == 'oz')
-	{
-		returnPackage.unit = 'g';
-	}
-
-	// fl-oz, cup, quart, tsp, tbsp to ml
-	else if ((request.body.unit == 'fl-oz') ||
-					 (request.body.unit == 'cup') ||
-					 (request.body.unit == 'qt') ||
-					 (request.body.unit == 'tsp') ||
-					 (request.body.unit == 'tbsp'))
-	{
-		returnPackage.unit = 'ml';
-	}
-
-	// gallon to liter
-	else if (request.body.unit == 'gal')
-	{
-		returnPackage.unit = 'l';
-	}
-
-	// farenheit to celsius
-	else if (request.body.unit == 'f')
-	{
-		returnPackage.unit = 'c';
-	}
-
-	// Unit could not be converted
-	else
-	{
-		returnPackage.error = 'Unit could not be compared.';
-	}
-
-	response.status(200).json(returnPackage);
-});
-
-
-// Matches metric to imperial unit for conversion package
-app.post('/fromMetricToImperial', async (request, response, next) =>
-{
-	/*
-		Incoming:
-		{
-			unit : string
-		}
-
-		Output:
-		{
-			unit : string,
-			error : string
-		}
-	*/
-
-	var returnPackage = {
-												unit : '',
-												error : ''
-											};
-	
-	// ml to tsp
-	if (request.body.unit == 'ml')
-	{
-		returnPackage.unit = 'tsp';
-	}
-
-	// l to gallons
-	else if (request.body.unit == 'l')
-	{
-		returnPackage.unit = 'gal';
-	}
-
-	// g to oz
-	else if (request.body.unit == 'g')
-	{
-		returnPackage.unit = 'oz';
-	}
-
-	// kg to lb
-	else if (request.body.unit == 'kg')
-	{
-		returnPackage.unit = 'lb';
-	}
-
-	// c to f
-	else if (request.body.unit == 'c')
-	{
-		returnPackage.unit = 'f';
-	}
-
-	// Unit could not be converted
-	else
-	{
-		returnPackage.error = 'Unit could not be compared.';
-	}
-
-
-	response.status(200).json(returnPackage);
-});
-
-
-// Register Endpoint
-app.post('/registerUser', async (request, response, next) =>
-{
-  // incoming: userId, color
-  // outgoing: userID, username, email, firstName, lastName, profilePicture
-  // isVerified, favoriteRecipes, error
-	
-	const { username,
-					password,
-					email,
-					firstName,
-					lastName,
-					profilePicture
-				} = request.body;
-
-  const INVALID_USER = -1;
+  var searchUser = {
+					_id : ObjectID(request.body._id)
+  };
   var result = null;
 
   var returnPackage = {
-                        userID : INVALID_USER,
-                        username : '',
-                        email : '',
-                        firstName : '',
-                        lastName : '',
-                        profilePicture : profilePicture,
-                        isVerified : false,
-                        favoriteRecipes : [],
-                        error : ''
-                      };
+						success : false,
+						error : ''
+					  };
+					  
+					  
 
-  const newUser = {
-                    username : username, 
-                    password : password, 
-                    email : email,
-                    firstName : firstName,
-                    lastName : lastName,
-                    profilePicture : profilePicture,
-                    isVerified : false,
-                    favoriteRecipes : []
-                  };
-
-  // Check if user name is already taken.
-  try 
-  {
-		const db = client.db(process.env.APP_DATABASE);
-		
-    const criteria = {
-                        username : username
-                      };
-		const data = await db.collection(process.env.COLLECTION_USERS).findOne(criteria);
-		
-    if (data)
-    {
-      throw "User name already taken.";
-    }
-  }
-  catch(e)
-  {
-    returnPackage.error = e.toString();
-    response.status(400).json(returnPackage);
-    return;
-  }
-
-  // Inserts new user record. 
+  // Delete recipe record. 
   try
   {
     const db = client.db(process.env.APP_DATABASE);
-    db.collection(process.env.COLLECTION_USERS).insertOne(newUser);
-
-		var result = await db.collection(process.env.COLLECTION_USERS).findOne(newUser);
+	await db.collection(process.env.COLLECTION_RECIPES).deleteOne(searchUser);
+	var result = await db.collection(process.env.COLLECTION_RECIPES).findOne(searchUser);
   }
   catch(e)
   {
@@ -426,21 +70,17 @@ app.post('/registerUser', async (request, response, next) =>
     response.status(500).json(returnPackage);
     return;
   }
-
+  
+  console.log( result);
   // Assigns return package values. 
-  if (result)
+  if (!result)
   {
-    returnPackage.userID = result.userID;
-    returnPackage.username = result.username;
-    returnPackage.email = result.email;
-    returnPackage.firstName = result.firstName;
-    returnPackage.lastName = result.lastName;
-    returnPackage.profilePicture = result.profilePicture;
-    returnPackage.isVerified = result.isVerified;
-    returnPackage.favoriteRecipes = result.favoriteRecipes;
-  }
+    
+    returnPackage.success = true;
+    // console.log(returnPackage.success);
+	}
+
   response.status(200).json(returnPackage);
-}
-);
+});
 
 app.listen(process.env.PORT || 5000); // start Node + Express server on port 5000
