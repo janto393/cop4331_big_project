7fbc4eff3811ffdbc6139c00bf8501f663fafcb2
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-11-20 20:57:22 -0500

    formatted some of the return html.

--- front-end/src/components/Logout.js
+++ front-end/src/components/Logout.js
@@ -4,17 +4,16 @@ import './SidebarFiles/Sidebar.css';
 
 function Logout()
 {
-    const doLogout = event => 
-    {
-      event.preventDefault();
-        localStorage.removeItem("user_data")
-        window.location.href = '/';
-    };
+	const doLogout = event => 
+	{
+		event.preventDefault();
+		localStorage.removeItem("user_data")
+		window.location.href = '/';
+	};
 
   return(
     <div>
-        <Button variant = "light" id="logoutButton" className="side-menu-items" 
-            onClick={doLogout}> Log Out </Button>
+			<Button variant = "light" id="logoutButton" className="side-menu-items" onClick={doLogout}> Log Out </Button>
    </div>
   );
 
