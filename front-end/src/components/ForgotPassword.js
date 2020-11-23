a21fd7c7467b11c5060c1950f805210a45106a44
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-11-23 03:17:29 -0500

    started functionality for the forgot password

---
+++ front-end/src/components/ForgotPassword.js
@@ -0,0 +1,25 @@
+// React imports
+import React from 'react';
+import Form from 'react-bootstrap';
+
+// CSS imports
+import './ForgotPassword.css';
+
+class ForgotPassword extends React.Component
+{
+	constructor()
+	{
+		super();
+	}
+
+	render()
+	{
+		return (
+			<div className="forgot-password-dialog">
+				
+			</div>
+		);
+	}
+}
+
+export default ForgotPassword;
