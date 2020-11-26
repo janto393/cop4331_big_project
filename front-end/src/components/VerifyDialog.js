394c5f4e50da2b01a11001e2429c2b4b03d7bd35
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-11-26 02:39:26 -0500

    Re-initialized branch with changes to new master.

---
+++ front-end/src/components/VerifyDialog.js
@@ -0,0 +1,35 @@
+// React imports
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+// CSS imports
+import './VerifyDialog.css';
+
+// enviornment variables
+const PORT = (process.env.PORT || 5000);
+
+class VerifyDialog extends React.Component
+{
+	constructor()
+	{
+		super();
+		this.message;
+	}
+
+	componentDidMount()
+	{
+		
+	}
+
+	render()
+	{
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+export default VerifyDialog;
