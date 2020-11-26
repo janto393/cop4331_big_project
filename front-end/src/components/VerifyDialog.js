3322735e25f0d9e7fa63e02939e5051cda2187ce
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-11-26 02:53:50 -0500

    Finished implementing template for joeseph to work on

--- front-end/src/components/VerifyDialog.js
+++ front-end/src/components/VerifyDialog.js
@@ -14,19 +14,33 @@ class VerifyDialog extends React.Component
 	constructor()
 	{
 		super();
-		this.message;
+
+		// hook to store the recipe
+		this.state = {recipe : {}};
+
+		// container for any message needing to be displayed in the component
+		this.message = '';
 	}
 
 	componentDidMount()
 	{
-		
+		// bring the URI to local scope
+		const uri = window.location;
+
+		// using regex to extract the parameter out of the URI
+		const regex = /id=[[a-zA-Z0-9]+/;
+		const extracted = regex.exec(uri);
+
+		const userID = extracted[0].slice(3);
+
+		// TODO: fetch API (look at view Recipe for guidance)
 	}
 
 	render()
 	{
 		return (
 			<div>
-
+				<Link to="/">Go Back</Link>
 			</div>
 		);
 	}
