6da98acfc22e35cf7d64de8c9f6fa7fe1fe150ad
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-11-26 19:32:05 -0500

    Added search bar to home page on front-end.

--- front-end/src/pages/RecipesPage.js
+++ front-end/src/pages/RecipesPage.js
@@ -3,15 +3,15 @@ import React from 'react';
 
 // Component imports
 import RecipeCards from '../components/RecipeCards';
-import Searchbar from '../components/Searchbar';
 
 // jwt imports
 import jwt_decode from 'jwt-decode';
+import { Form } from 'react-bootstrap';
 
 // environment variables
 const PORT = (process.env.PORT || 5000);
 
-function fetchRecipes()
+function fetchRecipes(title, category)
 {
 	// Create a criteria package that will return any recipes
 	var criteria = {
@@ -21,6 +21,18 @@ function fetchRecipes()
 		userID : '',
 		currentPage : 1,
 		pageCapacity : 10
+	};
+
+	// filter title
+	if (title !== '')
+	{
+		criteria.title = title;
+	}
+
+	// filter category
+	if (category !== 'Any Category')
+	{
+		criteria.category = category;
 	}
 
 	// fetch the recipes from the database
@@ -47,6 +59,8 @@ class RecipesPage extends React.Component
 	{
 		super();
 		this.state = {data : []};
+		this.category = 'Any Category';
+		this.title = '';
 	}
 
 	componentDidMount()
@@ -58,9 +72,50 @@ class RecipesPage extends React.Component
 
 	render()
 	{
+		const search = () =>
+		{
+			fetchRecipes(this.title, this.category)
+				.then(response => response.json())
+				.then(json => this.setState({data : jwt_decode(json).recipes}));
+		};
+
+		const handleCategoryChange = (cat) =>
+		{
+			this.category = cat.target.value;
+			search();
+		};
+
+		const handleTitleChange = (title) =>
+		{
+			this.title = title.target.value;
+			search();
+		}
+
 		return (
 			<div>
-				<Searchbar />
+				<div className="searchbar-div">
+					<Form>
+						<Form.Row>
+
+							<Form.Group>
+								<Form.Control type="text" placeholder="Recipe Title" onChange={handleTitleChange} />
+							</Form.Group>
+							
+							<Form.Group>
+								<Form.Control as="select" onChange={handleCategoryChange}>
+									<option>Any Category</option>
+									<option>Breakfast</option>
+									<option>Lunch</option>
+									<option>Dinner</option>
+									<option>Desert</option>
+									<option>Drinks</option>
+									<option>Snacks</option>
+								</Form.Control>
+							</Form.Group>
+
+						</Form.Row>
+					</Form>
+				</div>
 				<RecipeCards recipes={this.state.data} />
 			</div>
 		);
