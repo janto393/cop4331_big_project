2066aa3e4cd2d4c5cbf4e50f81efd3f56e0809f9
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-11-30 01:43:23 -0500

    fixed front-end directory names

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ client.connect();
 if (process.env.NODE_ENV === 'production') 
 {
   // Set static folder
-  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.use(express.static(path.join(__dirname, 'front-end', 'build')));
 
   app.get('*', (req, res) => 
 	{
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'));
 	});
 }
 
